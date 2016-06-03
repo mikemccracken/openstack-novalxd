2a5193a535dd221abb38294f4eb8aa3c5cac5a0c
Adam Stokes <adam.stokes@ubuntu.com>
2016-06-03 17:07:20 -0400

    update checkers
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- conjure/steps/00_deploy-done.sh
+++ conjure/steps/00_deploy-done.sh
@@ -4,12 +4,28 @@
 
 . /usr/share/conjure-up/hooklib/common.sh
 
-if [ $(unitStatus keystone 0) = "error" ]; then
-    exposeResult "Error with keystone, please check juju status" 1 "false"
-fi
+declare -a services=("keystone" "glance" "neutron-api" "neutron-gateway" "ceph-mon" "ceph-osd" "ceph-radosgw" "cinder" "cinder-ceph" "mysql" "neutron-openvswitch" "nova-cloud-controller" "nova-compute" "lxd" "openstack-dashboard" "rabbitmq-server")
 
-if [ $(unitStatus keystone 0) != "active" ]; then
-    exposeResult "keystone not quite ready yet" 0 "false"
-fi
+check_error() {
+    for i in "${services[@]}"
+    do
+        if [ $(unitStatus $i 0) = "error" ]; then
+            exposeResult "Error with $i, please check juju status" 1 "false"
+        fi
+    done
+}
+
+
+check_active() {
+    for i in "${services[@]}"
+    do
+        if [ $(unitStatus $i 0) != "active" ]; then
+            exposeResult "$i not quite ready yet" 0 "false"
+        fi
+    done
+}
+
+check_error
+check_active
 
 exposeResult "All services started and ready" 0 "true"
