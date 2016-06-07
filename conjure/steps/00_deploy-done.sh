ee5c1ad52e70a4b7c7630ef714334b222510a3ac
Adam Stokes <adam.stokes@ubuntu.com>
2016-06-07 10:55:33 -0400

    update checker
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- conjure/steps/00_deploy-done.sh
+++ conjure/steps/00_deploy-done.sh
@@ -18,28 +18,7 @@ services=("keystone" \
               "openstack-dashboard" \
               "rabbitmq-server")
 
-check_error() {
-    for i in "${services[@]}"
-    do
-        if [ $(unitStatus $i 0) = "error" ]; then
-            debug "$i, gave a charm error."
-            exposeResult "Error with $i, please check juju status" 1 "false"
-        fi
-    done
-}
+checkUnitsForErrors $services
+checkUnitsForActive $services
 
-
-check_active() {
-    for i in "${services[@]}"
-    do
-        debug "Checking agent state of $i: $(unitStatus $i 0)"
-        if [ $(unitStatus $i 0) != "active" ]; then
-            exposeResult "$i not quite ready yet" 0 "false"
-        fi
-    done
-}
-
-check_error
-check_active
-
-exposeResult "All services started and ready" 0 "true"
+exposeResult "Applications ready" 0 "true"
