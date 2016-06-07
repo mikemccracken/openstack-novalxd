96ee4aa081553d1889eadff0f0d1c80454a8bbe2
Adam Stokes <adam.stokes@ubuntu.com>
2016-06-07 10:51:21 -0400

    update horizon
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- conjure/steps/05_horizon.sh
+++ conjure/steps/05_horizon.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #
-# Title: Display Horizon
+# Title: Configuring Horizon
 
 . /usr/share/conjure-up/hooklib/common.sh
+
+dashboard_address() {
+    unitAddress openstack-dashboard 0
+}
 
 exposeResult "Login to Horizon: http://$(dashboard_address)/horizon l: admin p: openstack" 0 "true"
