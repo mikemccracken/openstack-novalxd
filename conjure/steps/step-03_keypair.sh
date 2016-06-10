bb11abd61d1c19a7cbda70aa3a5571d2d6cf8123
Adam Stokes <adam.stokes@ubuntu.com>
2016-06-10 18:50:19 -0400

    better handling of ssh keypair
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- conjure/steps/step-03_keypair.sh
+++ conjure/steps/step-03_keypair.sh
@@ -3,15 +3,15 @@
 . /usr/share/conjure-up/hooklib/common.sh
 
 . $SCRIPTPATH/novarc
-if [[ $JUJU_PROVIDERTYPE =~ "lxd" ]]; then
 
-    if [ ! -f $SSHPUBLICKEY ]; then
-        debug "Adding ssh public key $SSHPUBLICKEY"
-        . $SCRIPTPATH/novarc
-        if ! openstack keypair show ubuntu-keypair > /dev/null 2>&1; then
-            while ! openstack keypair create --public-key $HOME/.ssh/id_rsa.pub ubuntu-keypair > /dev/null 2>&1; do sleep 5; done
-            exposeResult "Added SSH Keypair" 0 "true"
-        fi
+if [ ! -f $SSHPUBLICKEY ]; then
+    debug "Adding ssh public key $SSHPUBLICKEY"
+    . $SCRIPTPATH/novarc
+    if ! openstack keypair show ubuntu-keypair > /dev/null 2>&1; then
+        while ! openstack keypair create --public-key $HOME/.ssh/id_rsa.pub ubuntu-keypair > /dev/null 2>&1; do sleep 5; done
+        exposeResult "Added SSH Keypair" 0 "true"
+    else
+        exposeResult "A keypair already exists for ubuntu-keypair, did not need to re-import." 0 "true"
     fi
-    exposeResult "Unable to add public ssh key, maybe ssh-keygen needs to be run." 1 "false"
 fi
+exposeResult "Unable to add public ssh key, maybe ssh-keygen needs to be run." 1 "false"
